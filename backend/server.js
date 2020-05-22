3bf0a8aaec82e3eafe3eaf0e6cea0d6376828b1f
jenniferot <56264114+Jenniferot@users.noreply.github.com>
2020-05-22 23:58:23 +0200

    started with backend

--- backend/server.js
+++ backend/server.js
@@ -2,14 +2,34 @@ import express from 'express'
 import bodyParser from 'body-parser'
 import cors from 'cors'
 import mongoose from 'mongoose'
+import crypto from 'crypto'
+import bcrypt from 'bcrypt-nodejs'
 
 const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/authAPI"
 mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true })
 mongoose.Promise = Promise
+mongoose.set('useCreateIndex', true)
 
-// Defines the port the app will run on. Defaults to 8080, but can be 
-// overridden when starting the server. For example:
-//
+
+const User = mongoose.model('User', {
+  name: {
+    type: String,
+    unique: true,
+  },
+  email: {
+    type: String,
+    required: true
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  accessToken: {
+    type: String,
+    default: () => crypto.randomBytes(128).toString('hex')
+  }
+})
+
 //   PORT=9000 npm start
 const port = process.env.PORT || 8080
 const app = express()
@@ -23,6 +43,66 @@ app.get('/', (req, res) => {
   res.send('Hello world')
 })
 
+const authenticateUser = async (req, res, next) => {
+  try {
+    const user = await User.findOne({
+      accessToken: req.header('Authorization')
+    });
+    if (user) {
+      req.user = user;
+      next();
+    } else {
+      res.status(401).json({ loggedOut: true })
+    }
+  } catch (err) {
+    res
+      .status(403)
+      .json({ message: 'access token missing or wrong', errors: err.errors })
+  }
+};
+
+// Create user - sign up
+app.post('/users', async (req, res) => {
+  try {
+    const { name, email, password } = req.body;
+    const user = new User({ name, email, password: bcrypt.hashSync(password) })
+    const saved = await user.save()
+    res.status(201).json(saved)
+  } catch (err) {
+    res.status(400).json({ message: 'Could not save user', errors: err.errors })
+  }
+})
+
+app.get('/users/:id', authenticateUser)
+app.get('/users/:id', (req, res) => {
+  res.send('Yes');
+});
+
+//Login user - sign in
+app.post('/sessions', async (req, res) => {
+  try {
+    const { name, password } = req.body
+    const user = await User.findOne({ name })
+    if (user && bcrypt.compareSync(password, user.password)) {
+      res.status(201).json({ userId: user._id, accessToken: user.accessToken })
+    } else {
+      res.json({ notFound: true })
+    }
+  } catch (err) {
+    res.status(400).json({ message: 'could not find user', errors: err.errors })
+  }
+})
+
+/*
+//Login user
+app.post('/sessions', async (req, res) => {
+  const user = await User.findOne({ email: req.body.email });
+  if (user && bcrypt.compareSync(req.body.password, user.password)) {
+    res.json({ userId: user._id, accessToken: user.accessToken });
+  } else {
+    res.json({ notFound: true });
+  }
+});*/
 // Start the server
 app.listen(port, () => {
   console.log(`Server running on http://localhost:${port}`)
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,90 @@
-import React from 'react'
+import React, { useState } from 'react'
+
+const URL = 'http://localhost:8080/users'
 
 export const App = () => {
+  const [name, setName] = useState('')
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+
+  const handleSubmit = event => {
+    event.preventDefault()
+
+    fetch(URL, {
+      method: 'POST',
+      body: JSON.stringify({ name, password }),
+      headers: { 'Content-Type': 'application/json' }
+    })
+      .then(res => res.json())
+      .then(json => console.log(json))
+      .catch(err => console.log('error:', err))
+  }
+
   return (
     <div>
-      Find me in src/app.js!
+      <form onSubmit={handleSubmit}>
+        <h1>Sign up</h1>
+        <label>
+          name
+          <input
+            required
+            value={name}
+            onChange={event => setName(event.target.value)}
+          />
+        </label>
+        <label>
+          email
+          <input
+            required
+            value={email}
+            onChange={event => setEmail(event.target.value)}
+          />
+        </label>
+        <label>
+          password
+          <input
+            required
+            value={password}
+            onChange={event => setPassword(event.target.value)}
+          />
+        </label>
+        <button type="submit" onClick={handleSubmit}>
+          SIGN UP
+          </button>
+      </form>
+      <div>
+        <form onSubmit={handleSubmit}>
+          <h1>Sign in</h1>
+          <label>
+            name
+          <input
+              required
+              value={name}
+              onChange={event => setName(event.target.value)}
+            />
+          </label>
+          <label>
+            email
+          <input
+              required
+              value={email}
+              onChange={event => setEmail(event.target.value)}
+            />
+          </label>
+          <label>
+            password
+          <input
+              required
+              value={password}
+              onChange={event => setPassword(event.target.value)}
+            />
+          </label>
+          <button type="submit" onClick={handleSubmit}>
+            SIGN UP
+          </button>
+        </form>
+      </div>
     </div>
+
   )
 }
